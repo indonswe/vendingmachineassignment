6b4786a02ecaa7477b702a4c8c64b99bd9c74376
indonswe <afc2002afc@hotmail.com>
2020-11-23 10:03:58 +0100

    pMM

--- src/main/java/org/example/mainMenu.java
+++ src/main/java/org/example/mainMenu.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class mainMenu {
     boolean quit=false;
     String answer;
+    int answerInt;
     public mainMenu (){
     }
-    public void pMM(){
+    public int pMM(){
         System.out.println("1. Put in money");
         System.out.println("2. Choose products");
         System.out.println("3. Quit");
-        wFA();
+        answer = wFA();
+        answerInt = Integer.parseInt(answer);
+        return answerInt;
     }
-    public void wFA(){
+    public String wFA(){
         Scanner scNr1 = new Scanner(System.in);
         answer = scNr1.nextLine(); // holds actual input
+        return answer;
     }
 }
