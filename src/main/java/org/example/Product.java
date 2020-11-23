415a278694d2363f344fe4ca401a97d46c31a55b
indonswe <afc2002afc@hotmail.com>
2020-11-23 15:13:53 +0100

    abstract

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,5 +1,34 @@
 package org.example;
 
-public class Product {
+abstract class Product {
+    public abstract String examine();
+    public abstract String use();
+}
+class chocolade extends Product{
 
+    @Override
+    public String examine(){
+        return null;
+    }
+
+    @Override
+    public String use() {
+        return null;
+    }
+}
+class snickers extends chocolade{
+
+    @Override
+    public String examine(){
+        return toString("Pris 5 kr, vikt 100 gram, kalorier 50 kcal.");
+    }
+
+    private String toString(String s) {
+        return s;
+    }
+
+    @Override
+    public String use() {
+        return null;
+    }
 }
